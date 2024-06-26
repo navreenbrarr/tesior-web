2cfc6d71aa2ce3748c5e455c29ee02e4c6765b55
jogeshwar.singh <jogeshwar.singh@greyb.com>
2024-06-27 00:41:32 +0530

    hardcode connection

--- apps/next-web/lib/redis/queues.ts
+++ apps/next-web/lib/redis/queues.ts
@@ -1,44 +1,24 @@
-import { QueueOptions } from "bull";
-import { Redis } from "ioredis";
-import { REDIS_CONNECTION_URL } from "./config";
+
 import { processUserPaymentQueue, processAdminEscrowQueue } from "./process";
 import Queue from "bull";
 
-const queueOptions: QueueOptions = {
-  redis: REDIS_CONNECTION_URL,
-  defaultJobOptions: {
-    attempts: 2,
-    backoff: {
-      type: "exponential",
-      delay: 50000,
-    },
+export const userPayoutQueue = new Queue("user-payout-queue", {
+  redis: {
+    port: 6379,
+    host: "relative-poodle-38122.upstash.io",
+    password: "AZTqAAIncDE2Njc2NTFmYTQxYTI0ZjQwOWFhYTM0MGIyYzQyNDNlY3AxMzgxMjI",
+    tls: {},
   },
-};
-
-export const userPayoutQueue = new Queue("user-payout-queue", queueOptions);
-export const adminEscrowQueue = new Queue("admin-escrow-queue", queueOptions);
+});
+export const adminEscrowQueue = new Queue("admin-escrow-queue", {
+  redis: {
+    port: 6379,
+    host: "relative-poodle-38122.upstash.io",
+    password: "AZTqAAIncDE2Njc2NTFmYTQxYTI0ZjQwOWFhYTM0MGIyYzQyNDNlY3AxMzgxMjI",
+    tls: {},
+  },
+});
 
 // Register job processors
 userPayoutQueue.process(processUserPaymentQueue);
 adminEscrowQueue.process(processAdminEscrowQueue);
-
-// Optional: Listening to job events for logging
-userPayoutQueue.on("waiting", (jobId: any) => {
-  console.log("Waiting user payout", jobId);
-});
-userPayoutQueue.on("completed", (job: { id: any; }) => {
-  console.log("Completed user payout", job.id);
-});
-userPayoutQueue.on("failed", (job: { id: any; }, error: any) => {
-  console.error("Error in user payout", job.id, error);
-});
-
-adminEscrowQueue.on("waiting", (jobId: any) => {
-  console.log("Waiting admin escrow", jobId);
-});
-adminEscrowQueue.on("completed", (job: { id: any; }) => {
-  console.log("Completed admin escrow", job.id);
-});
-adminEscrowQueue.on("failed", (job: { id: any; }, error: any) => {
-  console.error("Error in admin escrow", job.id, error);
-});
